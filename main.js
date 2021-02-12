ef314746e1d51671db187f05e558d5a959b49ae3
Warren Smyth <warrensmyth@me.com>
2021-02-12 13:44:25 -0600

    Pretty up the UI

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 let Phrase = require("wsmyth-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester() {
+  let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(string);
+  let palindromeResult = document.querySelector("#palindromeResult");
 
-if (phrase.palindrome()) {
-	alert(`"${phrase.content}" is a palindrome!`);
-} else {
-	alert(`"${phrase.content}" is not a palindrome.`);
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let button = document.querySelector("#palindromeTester");
+  button.addEventListener("click", function() {
+    palindromeTester();
+  });
+});
